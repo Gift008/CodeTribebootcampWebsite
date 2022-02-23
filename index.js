178fcca690adb85dee86c1848c659ed6919f062b
Your Name <maakegift008@gmail.com>
2022-02-23 11:52:02 +0200

    3rd Commint JS

---
+++ index.js
@@ -0,0 +1,27 @@
+let userInfo = {
+    //Personal information
+    username: "Gift",
+    surname: "Maake",
+    email: "maakegift008@gmail.com",
+    Phonenumber: "0737460380",
+    location: "Gauteng,Johannesburg",
+
+    //Educationa Background
+    highschool: "Napscom Secondary",
+    university: "University of Limpopo",
+    qualification: "BSc Mathematical Sciences (Major Computer Sciences & Applied Mathematics)",
+
+    //Working Experience
+    companyname: "Waraco PTY LTD",
+    companylocation: "Sandton, Sandawn",
+    startdate: 13/09/2021,
+    enddate: 05/01/2022,
+    terminationreason: "Retrentched",
+    
+    //Projects I have done in the past
+    
+
+
+
+
+}
